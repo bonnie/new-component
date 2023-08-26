2e5888e6b82f817b90751c7fa3a37e87b22e14ac
Bonnie Schulkin <biz@bonnieschulkin.com>
2023-08-26 10:49:20 -0700

    Add styles object to template

--- src/templates/ts.tsx
+++ src/templates/ts.tsx
@@ -4,9 +4,11 @@ import { css } from "~/styled-system/css";
 export type COMPONENT_NAMEProps = {};
 
 function COMPONENT_NAME({}: COMPONENT_NAMEProps) {
-  return <div className={wrapper}></div>;
+  return <div className={styles.wrapper}></div>;
 }
 
-const wrapper = css({});
+const styles = {
+  wrapper: css({}),
+};
 
 export default COMPONENT_NAME;
